8a7fa98dd309c2c3ac5829fd6ad05c4fb831c54e
koa102025-cmd <koa102025@gmail.com>
2025-12-15 12:37:38 +0100

    design

---
+++ assets/emojies/text.md
@@ -0,0 +1,8 @@
+Colours:
+#0fa3b1
+#d9e5d6
+#eddea4
+#f7a072
+#ff9b42
+
+Font-family: Coiny
---
+++ javascript.js
@@ -0,0 +1,11 @@
+const emojies = [
+	"emojies/angel.png",
+	"emojies/angry.png",
+	"emojies/cool.png",
+	"emojies/hands.png",
+	"emojies/laugh.png",
+	"emojies/shocked.png",
+];
+
+let firstCard = 0;
+let secondCard = 0;
