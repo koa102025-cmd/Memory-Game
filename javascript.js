0041a3f70ac8c201690755129fb5fc9f526d3bee
koa102025-cmd <koa102025@gmail.com>
2025-12-16 09:46:27 +0100

    2 cards check + matches

--- javascript.js
+++ javascript.js
@@ -14,14 +14,11 @@ const emojies = [
 let firstCard = 0;
 let secondCard = 0;
 let lock = false;
-// let matches = 0;
-// let moves = 0;
-// let seconds = 0;
-// let timeRunning = false;
-// let timerInterval;
+let match = 0;
 
 const cards = document.querySelectorAll(".card");
 const newGameBtn = document.getElementById("new-game");
+const matches = document.getElementById("matches");
 
 //shuffle array
 function shuffle(array) {
@@ -47,15 +44,54 @@ cards.forEach((card) => {
 	card.addEventListener("click", () => flipCard(card));
 });
 
+//changing backround image style of the card
 function flipCard(card) {
+	if (firstCard === card) return;
+	if (lock) return;
+
 	const emojiDiv = card.querySelector(".icon-hover");
 
 	emojiDiv.style.backgroundImage = `url(./assets/${card.dataset.emoji})`;
+	if (!firstCard) {
+		firstCard = card;
+		return;
+	}
+	secondCard = card;
+	lock = true;
+	checkMatch();
 }
 
-newGameBtn.addEventListener("click", renewCards);
+function checkMatch() {
+	const isMatch = firstCard.dataset.emoji === secondCard.dataset.emoji;
 
-function renewCards() {
+	if (isMatch) {
+		match++;
+		matches.textContent = `${match}/5`;
+
+		// fCard = 0; sCard = 0; lock = 0
+		resetTurn();
+	} else {
+		setTimeout(() => {
+			firstCard.querySelector(
+				".icon-hover"
+			).style.backgroundImage = `url(./assets/black-puzzle.png)`;
+			secondCard.querySelector(
+				".icon-hover"
+			).style.backgroundImage = `url(./assets/black-puzzle.png)`;
+			resetTurn();
+		}, 800);
+	}
+}
+
+function resetTurn() {
+	firstCard = 0;
+	secondCard = 0;
+	lock = false;
+}
+
+newGameBtn.addEventListener("click", updateCards);
+
+function updateCards() {
 	cards.forEach((card) => {
 		const emojiDiv = card.querySelector(".icon-hover");
 
